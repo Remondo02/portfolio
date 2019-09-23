93cba15117ccfdeaa0918cc198119c8db82643b0
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2019-09-23 23:41:31 +0200

    Add languages PHP files & SVG flags in footer

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include "config.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -17,10 +21,10 @@
             <h4>Rémi Meullemeestre</h4>
           </div>
           <ul class="nav-links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#portfolio">Portfolio</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#home"><?php echo $lang['home'] ?></a></li>
+            <li><a href="#about"><?php echo $lang['about'] ?></a></li>
+            <li><a href="#portfolio"><?php echo $lang['portfolio'] ?></a></li>
+            <li><a href="#contact"><?php echo $lang['contact'] ?></a></li>
           </ul>
           <div class="burger">
             <div class="line1"></div>
@@ -86,7 +90,7 @@
                 </svg>
               </h1>
               <h2 class="home__details__job">
-                Fullstack Developer & Designer
+              <?php echo $lang['job'] ?>
               </h2>
               <div class="icons">
                 <a href="https://www.linkedin.com/in/remimeullemeestre/">
@@ -122,7 +126,7 @@
                   <aside class="quote">
                     <blockquote>
                       <p>
-                          C'est bien dans l'effort que l'on trouve la satisfaction et non dans la réussite. Un plein effort est une pleine victoire.
+                      <?php echo $lang['quote'] ?>
                       </p>
                       <cite>Ghandi</cite>
                     </blockquote>
@@ -136,22 +140,22 @@
                   <h3>Profil</h3>
                   <ul class="about__details__content__lists__single">
                     <li>
-                      <strong>Fullname:</strong>
+                      <strong><?php echo $lang['name'] ?></strong>
                       <span>Rémi Meullemeestre</span>
                     </li>
                     <li>
-                      <strong>Birth Date:</strong>
+                      <strong><?php echo $lang['birthdate'] ?></strong>
                       <span>4 Octobre 1981</span>
                     </li>
                     <li>
-                      <strong>Email:</strong>
+                      <strong><?php echo $lang['email'] ?></strong>
                       <span>remi.meullemeestre@gmail.com</span>
                     </li>
                   </ul>
                   <!--/info-list-->
                 </div>
                 <div class="about__details__content__skills">
-                  <h3>Skills</h3>
+                  <h3><?php echo $lang['skill'] ?></h3>
 
                   <ul class="skill__bars">
                     <li>
@@ -180,13 +184,13 @@
               </div>
               <div class="about__details__contact">
                 <a href="#contact" title="Hire Me" class="button button__hire"
-                  >Hire Me</a
+                  ><?php echo $lang['hire'] ?></a
                 >
                 <a
                   href="docs/remi.meullemeestre.pdf"
                   title="Download CV"
                   class="button button__download"
-                  >Download CV</a
+                  ><?php echo $lang['download'] ?></a
                 >
               </div>
             </div>
@@ -202,12 +206,9 @@
                   <img src="./img/photo-gallery-1.jpg" alt="" />
                 </div>
                 <div class="portfolio__gallery__container__content">
-                  <h2>Image Hover Effects</h2>
+                  <h2><?php echo $lang['gallery title 1'] ?></h2>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Rerum impedit fugit quisquam molestiae maiores, obcaecati ea
-                    officiis, rem asperiores accusantium voluptatem. Magnam odio
-                    voluptates, quo quasi eius recusandae ab? Cumque.
+                  <?php echo $lang['gallery p 1'] ?>
                   </p>
                   <!-- <div class="details"><a href="" class="button__hire button__hire--img" title="portfolio">Details1</a></div> -->
                 </div>
@@ -217,13 +218,9 @@
                   <img src="./img/photo-gallery-2.jpg" alt="" />
                 </div>
                 <div class="portfolio__gallery__container__content">
-                  <h2>Image Hover Effects</h2>
+                  <h2><?php echo $lang['gallery title 2'] ?></h2>
                   <p>
-                    Accusamus est enim vitae, doloribus fuga laudantium vero
-                    assumenda, eius neque magni sed officiis necessitatibus
-                    alias voluptatum sapiente exercitationem tempore eligendi
-                    veritatis in impedit delectus dolorum commodi earum soluta?
-                    Corrupti?
+                  <?php echo $lang['gallery p 2'] ?>
                   </p>
                   <!-- <div class="details"><a href="" class="button__hire button__hire--img" title="portfolio">Details2</a></div> -->
                 </div>
@@ -233,12 +230,9 @@
                   <img src="./img/photo-gallery-3.jpg" alt="" />
                 </div>
                 <div class="portfolio__gallery__container__content">
-                  <h2>Image Hover Effects</h2>
+                  <h2><?php echo $lang['gallery title 3'] ?></h2>
                   <p>
-                    Qui quod distinctio at possimus officiis veritatis? Beatae
-                    provident reiciendis architecto saepe illum. Culpa
-                    repudiandae porro quis sunt aliquam fugit deserunt pariatur?
-                    Facilis quae nisi, exercitationem inventore harum sit et.
+                  <?php echo $lang['gallery p 3'] ?>
                   </p>
                   <!-- <div class="details"><a href="" class="button__hire button__hire--img" title="portfolio">Details3</a></div> -->
                 </div>
@@ -248,12 +242,9 @@
                   <img src="./img/photo-gallery-4.jpg" alt="" />
                 </div>
                 <div class="portfolio__gallery__container__content">
-                  <h2>Image Hover Effects</h2>
+                  <h2><?php echo $lang['gallery title 4'] ?></h2>
                   <p>
-                    Ipsa quam sequi blanditiis inventore, est, eius tempora nam,
-                    nihil dolorum dolorem quibusdam incidunt hic? Atque unde
-                    nihil sapiente alias autem obcaecati magni ad sed numquam,
-                    quod sunt facere est.
+                  <?php echo $lang['gallery p 4'] ?>
                   </p>
                   <!-- <div class="details"><a href="" class="button__hire button__hire--img" title="portfolio">Details4</a></div> -->
                 </div>
@@ -269,23 +260,23 @@
                 <div class="contact__info__icon__single">
                   <i class="fa fa-map-marker"></i>
                 </div>
-                <h5>Where to find me</h5>
+                <h5><?php echo $lang['where to find me'] ?></h5>
                 <p>
-                  Creil Oise / Hauts-de-France
+                  Creil, Oise / Hauts-de-France
                 </p>
               </div>
               <div class="contact__info__icon">
                 <div class="contact__info__icon__single">
                   <i class="fa fa-envelope"></i>
                 </div>
-                <h5>Email Me At</h5>
+                <h5><?php echo $lang['email me at'] ?></h5>
                 <p>remi.meullemeestre@gmail.com</p>
               </div>
               <div class="contact__info__icon">
                 <div class="contact__info__icon__single">
                   <i class="fa fa-phone"></i>
                 </div>
-                <h5>Call Me At</h5>
+                <h5><?php echo $lang['call me at'] ?></h5>
                 <p>Phone: (+33) 7 83 78 77 48</p>
               </div>
             </div>
@@ -304,7 +295,7 @@
                       required
                     />
                     <label for="name" class="label-name">
-                      <span class="content-name">Name</span>
+                      <span class="content-name"><?php echo $lang['label name'] ?></span>
                     </label>
                   </div>
                   <div class="form">
@@ -315,7 +306,7 @@
                       required
                     />
                     <label for="name" class="label-name">
-                      <span class="content-name">Email</span>
+                      <span class="content-name"><?php echo $lang['label email'] ?></span>
                     </label>
                   </div>
                 </div>
@@ -328,7 +319,7 @@
                       required
                     />
                     <label for="name" class="label-name">
-                      <span class="content-name">Subject</span>
+                      <span class="content-name"><?php echo $lang['label subject'] ?></span>
                     </label>
                   </div>
                   <div class="form form-message form-test">
@@ -341,7 +332,7 @@
                       required
                     ></textarea>
                     <label for="name" class="label-name">
-                      <span class="content-name content-name-ta">Message</span>
+                      <span class="content-name content-name-ta"><?php echo $lang['message'] ?></span>
                     </label>
                   
                   </div>
@@ -353,7 +344,7 @@
               <!-- <div class="social__container"> -->
               <div class="copyright">
                 <span> Copyright 2019 ©</span>
-                <span>Design by Rémi Meullemeestre</span>
+                <span><?php echo $lang['copyright'] ?></span>
               </div>
               <div class="social__container__right">
                 <ul class="social__container__right__items">
@@ -391,6 +382,115 @@
                 </ul>
                 <!--social-->
               </div>
+              <div class="languages">
+              <svg class="flag"version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+              viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+              <a xlink:href="index.php?lang=fr"> echo $lang['lang_fr'] ?>
+                <circle style="fill:#F0F0F0;" cx="256" cy="256" r="256"/>
+                <path style="fill:#D80027;" d="M512,256c0-110.071-69.472-203.906-166.957-240.077v480.155C442.528,459.906,512,366.071,512,256z"/>
+                <path style="fill:#0052B4;" d="M0,256c0,110.071,69.473,203.906,166.957,240.077V15.923C69.473,52.094,0,145.929,0,256z"/>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                </a>
+              </svg>
+              
+              
+              <svg class="flag"version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+                <a xlink:href="index.php?lang=en"> echo $lang['lang_en'] ?>
+                <circle style="fill:#F0F0F0;" cx="256" cy="256" r="256" />
+                <g>
+                <path style="fill:#0052B4;" d="M52.92,100.142c-20.109,26.163-35.272,56.318-44.101,89.077h133.178L52.92,100.142z"/>
+                <path style="fill:#0052B4;" d="M503.181,189.219c-8.829-32.758-23.993-62.913-44.101-89.076l-89.075,89.076H503.181z"/>
+                <path style="fill:#0052B4;" d="M8.819,322.784c8.83,32.758,23.993,62.913,44.101,89.075l89.074-89.075L8.819,322.784L8.819,322.784
+                  z"/>
+                <path style="fill:#0052B4;" d="M411.858,52.921c-26.163-20.109-56.317-35.272-89.076-44.102v133.177L411.858,52.921z"/>
+                <path style="fill:#0052B4;" d="M100.142,459.079c26.163,20.109,56.318,35.272,89.076,44.102V370.005L100.142,459.079z"/>
+                <path style="fill:#0052B4;" d="M189.217,8.819c-32.758,8.83-62.913,23.993-89.075,44.101l89.075,89.075V8.819z"/>
+                <path style="fill:#0052B4;" d="M322.783,503.181c32.758-8.83,62.913-23.993,89.075-44.101l-89.075-89.075V503.181z"/>
+                <path style="fill:#0052B4;" d="M370.005,322.784l89.075,89.076c20.108-26.162,35.272-56.318,44.101-89.076H370.005z"/>
+                </g>
+                <g>
+                <path style="fill:#D80027;" d="M509.833,222.609h-220.44h-0.001V2.167C278.461,0.744,267.317,0,256,0
+                  c-11.319,0-22.461,0.744-33.391,2.167v220.44v0.001H2.167C0.744,233.539,0,244.683,0,256c0,11.319,0.744,22.461,2.167,33.391
+                  h220.44h0.001v220.442C233.539,511.256,244.681,512,256,512c11.317,0,22.461-0.743,33.391-2.167v-220.44v-0.001h220.442
+                  C511.256,278.461,512,267.319,512,256C512,244.683,511.256,233.539,509.833,222.609z"/>
+                <path style="fill:#D80027;" d="M322.783,322.784L322.783,322.784L437.019,437.02c5.254-5.252,10.266-10.743,15.048-16.435
+                  l-97.802-97.802h-31.482V322.784z"/>
+                <path style="fill:#D80027;" d="M189.217,322.784h-0.002L74.98,437.019c5.252,5.254,10.743,10.266,16.435,15.048l97.802-97.804
+                  V322.784z"/>
+                <path style="fill:#D80027;" d="M189.217,189.219v-0.002L74.981,74.98c-5.254,5.252-10.266,10.743-15.048,16.435l97.803,97.803
+                  H189.217z"/>
+                <path style="fill:#D80027;" d="M322.783,189.219L322.783,189.219L437.02,74.981c-5.252-5.254-10.743-10.266-16.435-15.047
+                  l-97.802,97.803V189.219z"/>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                </a>
+              </svg>
+              
+
+
+              </div>
+              <!-- <div class="languages">
+              <a href="index.php?lang=fr"> echo $lang['lang_fr'] ?></a> | <a href="index.php?lang=fr">php echo $lang['lang_en'] ?></a>
+              </div> -->
               <!-- </div> -->
             </div>
 
