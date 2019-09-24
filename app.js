0a89734522a8145fa70b17e3c2de88fe35837cff
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2019-09-24 23:55:47 +0200

    GSAP JS home & JS cleanup

--- app.js
+++ app.js
@@ -1,175 +1,159 @@
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
+function init() {
 
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
+  document.addEventListener('DOMContentLoaded', function (event) {
+    window.addEventListener('load', function (e) {
 
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = ''
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      function homeTimeline() {
+
+        const tl = new TimelineMax();
+
+        tl.from('#logo', 2, {
+          opacity: 0,
+          scale: .5,
+          ease: Power2.easeOut
+        })
+          .staggerFrom('.home__details__job, .icons', 2, {
+            opacity: 0,
+            y: -40,
+            ease: Power2.easeInOut
+          }, 0.5, "+=1")
+          .fromTo('nav', 1, { y: '-100%' }, { y: '0%' })
       }
-    });
-    burger.classList.toggle('toggle');
+
+      homeTimeline();
+
+
+      function splitScroll() {
+
+        const controller = new ScrollMagic.Controller();
+
+        new ScrollMagic.Scene({
+          triggerElement: '.about',
+        })
+          .setClassToggle('.about__details__heading', 'fade-in')
+          .addTo(controller);
+
+        new ScrollMagic.Scene({
+          triggerElement: 'blockquote'
+        })
+          .setClassToggle('.about__details__content', 'fade-in')
+          .addTo(controller);
+
+        new ScrollMagic.Scene({
+          triggerElement: '.percent50',
+          triggerHook: 0.4
+        })
+          .setClassToggle('.about__details__contact', 'fade-in')
+          .addTo(controller);
+
+        new ScrollMagic.Scene({
+          triggerElement: '.about__details__contact',
+          triggerHook: 0.2
+        })
+          .setClassToggle('.portfolio__gallery', 'fade-in')
+          .addTo(controller);
+
+        new ScrollMagic.Scene({
+          triggerElement: '.contact'
+        })
+          .setClassToggle('.contact__info', 'fade-in')
+          .addTo(controller);
+
+        new ScrollMagic.Scene({
+          triggerElement: '.contact__info'
+        })
+          .setClassToggle('.form-content', 'fade-in')
+          .addTo(controller);
+
+        new ScrollMagic.Scene({
+          triggerElement: '.contact__info'
+        })
+          .setClassToggle('.form-header', 'fade-in')
+          .addTo(controller);
+
+        new ScrollMagic.Scene({
+          triggerElement: '.form-message'
+        })
+          .setClassToggle('input[type="submit"]', 'fade-in')
+          .addTo(controller);
+
+        new ScrollMagic.Scene({
+          triggerElement: '.form-message',
+          triggerHook: 0.4
+        })
+          .setClassToggle('.social', 'fade-in')
+          .addTo(controller);
+      }
+
+      splitScroll();
+
+      const navSlide = () => {
+
+        const burger = document.querySelector('.burger');
+        const nav = document.querySelector('.nav-links');
+        const navLinks = document.querySelectorAll('.nav-links li');
+
+        burger.addEventListener('click', () => {
+          nav.classList.toggle('nav-active');
+
+          navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+              link.style.animation = ''
+            } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+          });
+          burger.classList.toggle('toggle');
+        });
+      }
+
+      navSlide();
+
+      const smoothScroll = () => {
+
+        const scroll = new SmoothScroll('.navbar, .about__details__contact, a[href*="#"]', {
+          speed: 800
+        });
+
+        const selectElement = (element) => document.querySelector(element);
+        selectElement('.burger').addEventListener('click', () => {
+          selectElement('.burger').classList.toggle('active');
+        });
+      }
+
+      smoothScroll();
+
+      const lightBoxContainer = () => {
+
+        const lightbox = document.createElement('div');
+        lightbox.id = 'lightbox';
+        document.body.appendChild(lightbox);
+
+        const images = document.querySelectorAll('.portfolio__gallery__container img');
+        images.forEach(image => {
+          image.addEventListener('click', e => {
+            lightbox.classList.add('active');
+            const img = document.createElement('img');
+            img.src = image.src;
+            while (lightbox.firstChild) {
+              lightbox.removeChild(lightbox.firstChild)
+            }
+            lightbox.appendChild(img);
+          })
+        })
+
+        lightbox.addEventListener('click', e => {
+          if (e.target != e.currentTarget) {
+            return
+          }
+          lightbox.classList.remove('active');
+        })
+      }
+
+      lightBoxContainer();
+
+    }, false);
   });
 }
 
-navSlide();
-
-const scroll = new SmoothScroll('.navbar, .about__details__contact, a[href*="#"]', {
-  speed: 800
-});
-
-const selectElement = (element) => document.querySelector(element);
-
-selectElement('.burger').addEventListener('click', () => {
-  selectElement('.burger').classList.toggle('active');
-
-});
-
-
-const lightbox = document.createElement('div');
-lightbox.id = 'lightbox';
-document.body.appendChild(lightbox);
-
-const images = document.querySelectorAll('.portfolio__gallery__container img');
-images.forEach(image => {
-  image.addEventListener('click', e => {
-    lightbox.classList.add('active');
-    const img = document.createElement('img');
-    img.src = image.src;
-    while (lightbox.firstChild) {
-      lightbox.removeChild(lightbox.firstChild)
-    }
-    lightbox.appendChild(img);
-  })
-})
-
-lightbox.addEventListener('click', e => {
-  if (e.target != e.currentTarget) {
-    return
-  }
-  lightbox.classList.remove('active');
-})
-
-
-function splitScroll() {
-
-  const controller = new ScrollMagic.Controller();
-
-  new ScrollMagic.Scene({
-    triggerElement: '.about',
-  })
-    .setClassToggle('.about__details__heading', 'fade-in')
-    // .addIndicators({
-    //   name: 'fade about',
-    //   colorTrigger: 'black',
-    //   indent: 200,
-    //   colorStart: '#75c695'
-    // })
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-    triggerElement: 'blockquote'
-  })
-    .setClassToggle('.about__details__content', 'fade-in')
-    // .addIndicators({
-    //   name: 'fade content',
-    //   colorTrigger: 'black',
-    //   indent: 200,
-    //   colorStart: '#75c695'
-    // })
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-    triggerElement: '.percent50',
-    triggerHook: 0.4
-  })
-    .setClassToggle('.about__details__contact', 'fade-in')
-    // .addIndicators({
-    //   name: 'fade hire',
-    //   colorTrigger: 'black',
-    //   indent: 200,
-    //   colorStart: '#75c695'
-    // })
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-    triggerElement: '.about__details__contact',
-    triggerHook: 0.2
-  })
-    .setClassToggle('.portfolio__gallery', 'fade-in')
-    // .addIndicators({
-    //   name: 'fade portfolio',
-    //   colorTrigger: 'black',
-    //   indent: 200,
-    //   colorStart: '#75c695'
-    // })
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-    triggerElement: '.contact'
-  })
-    .setClassToggle('.contact__info', 'fade-in')
-    // .addIndicators({
-    //   name: 'fade contact info',
-    //   colorTrigger: 'black',
-    //   indent: 200,
-    //   colorStart: '#75c695'
-    // })
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-    triggerElement: '.contact__info'
-  })
-    .setClassToggle('.form-content', 'fade-in')
-    // .addIndicators({
-    //   name: 'fade form',
-    //   colorTrigger: 'black',
-    //   indent: 200,
-    //   colorStart: '#75c695'
-    // })
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-    triggerElement: '.contact__info'
-  })
-    .setClassToggle('.form-header', 'fade-in')
-    // .addIndicators({
-    //   name: 'fade form-header',
-    //   colorTrigger: 'black',
-    //   indent: 350,
-    //   colorStart: '#75c695'
-    // })
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-    triggerElement: '.form-message'
-  })
-    .setClassToggle('input[type="submit"]', 'fade-in')
-    // .addIndicators({
-    //   name: 'fade submit',
-    //   colorTrigger: 'black',
-    //   indent: 350,
-    //   colorStart: '#75c695'
-    // })
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-    triggerElement: '.form-message',
-    triggerHook: 0.4
-  })
-    .setClassToggle('.social', 'fade-in')
-    // .addIndicators({
-    //   name: 'fade social',
-    //   colorTrigger: 'black',
-    //   indent: 500,
-    //   colorStart: '#75c695'
-    // })
-    .addTo(controller);
-}
-
-splitScroll();
+init();
